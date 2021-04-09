b76aa59117e5b4ae4ea8d368084481b239fbb613
Jon <Jonathan.Kreiter@outlook.com>
2021-04-09 07:28:39 -0700

    small updates for deploying to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, 'localhost', (error) => {
+server.listen((process.env.PORT || 5000), 'localhost', (error) => {
 	if (error) throw error;
 	console.log('SERVER IS LIVE');
 	console.log(`LISTENING ON PORT ${process.env.PORT}`);
